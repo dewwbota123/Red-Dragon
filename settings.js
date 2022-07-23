7fccea3671fcb96cfdc49363b39a866adfd3ca68
dewwbota123 <109875333+dewwbota123@users.noreply.github.com>
2022-07-23 13:06:22 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,33 +19,33 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `My name is SUPER BOT🔥
+My Creator is dewanga theakshana😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94741131157'] //Owner number in aive msg
+global.premium = ['+94741131157'] //Owner Number info
+global.ownernomer = '+94741131157' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
+global.ownername = '😎G.dewanga' //Owner Name
+global.botname = ' ☬SUPER༒Bot࿐' //Bot Name
 
-global.button = '☰ GITHUB ☰' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://github.com/prabhasha2006' //Change Your Button's URL
+global.button = '☰ SUPER BOT GROUP ☰' //Costomize A Button Name In Alive Message
+global.btnurl = 'https://chat.whatsapp.com/HESSW4phURzF6GIFWtU91h' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = '☬SUPER༒bot࿐' //Bot Pacage Name
+global.author = '😎G.dewanga❯❯' //Author Name
 global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
 global.dragonemoji = '🧩' // Menu emoji
 global.footer = '©_Kumuthu.' //In list menu
-global.chatbotpre = 'true'  // Chat bot true/false
+global.chatbotpre = 'false'  // Chat bot true/false
 
 global.ig = 'https://github.com/prabhasha2006' //Github Account!
 
-global.mess = { // COSTOMIZE WITH YOUR LANGUAGE
+global.mess = SI{ // COSTOMIZE WITH YOUR LANGUAGE
 	
     success: 'Done ✅',
     admin: 'Only Admins can use it!😉',
